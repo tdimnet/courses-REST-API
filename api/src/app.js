a3aa130490378cd97a3e695ca70db7f9220d1289
Thomas Dimnet <thomas.dimnet@gmail.com>
2024-03-20 09:23:29 +0100

    Trying to implement swagger

--- api/src/app.js
+++ api/src/app.js
@@ -2,6 +2,8 @@ const cors = require('cors')
 const express = require("express");
 const jsonParser = require("body-parser").json;
 const mongoose = require("mongoose");
+const swaggerJsdoc = require('swagger-jsdoc')
+const swaggerUI = require('swagger-ui-express')
 
 const courses = require("./routes/courses");
 const users = require("./routes/users");
@@ -12,6 +14,7 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 app.use(jsonParser());
 app.use(cors())
+
 
 mongoose.connect(DB_URL);
 const db = mongoose.connection;
@@ -32,6 +35,41 @@ app.get("/", (req, res) => res.send("Hello, World!"))
 
 app.use("/api/v1/courses", courses);
 app.use("/api/v1/users", users);
+
+
+const options = {
+  definition: {
+    openapi: "3.1.0",
+    info: {
+      title: "LogRocket Express API with Swagger",
+      version: "0.1.0",
+      description:
+        "This is a simple CRUD API application made with Express and documented with Swagger",
+      license: {
+        name: "MIT",
+        url: "https://spdx.org/licenses/MIT.html",
+      },
+      contact: {
+        name: "LogRocket",
+        url: "https://logrocket.com",
+        email: "info@email.com",
+      },
+    },
+    servers: [
+      {
+        url: "http://localhost:3000",
+      },
+    ],
+  },
+  apis: ["./routes/*.js"],
+};
+
+const specs = swaggerJsdoc(options)
+app.use(
+  '/api-docs',
+  swaggerUI.serve,
+  swaggerUI.setup(specs, { explorer: true })
+)
 
 app.listen(PORT, function () {
   console.log(`Project is running on http://localhost:${PORT}`);
